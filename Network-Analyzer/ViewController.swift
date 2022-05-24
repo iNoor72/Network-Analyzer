fddd021340c026aec362a79126e44883fa0f5aa8
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-05-24 16:37:57 +0200

    Added outlets for the UI elements.

--- Network-Analyzer/ViewController.swift
+++ Network-Analyzer/ViewController.swift
@@ -9,6 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: IPv4 & IPv6 IBOutlets
+    @IBOutlet weak var ipv4_1: UILabel!
+    @IBOutlet weak var ipv4_2: UILabel!
+    @IBOutlet weak var ipv4_3: UILabel!
+    @IBOutlet weak var ipv6_1: UILabel!
+    @IBOutlet weak var ipv6_2: UILabel!
+    @IBOutlet weak var ipv6_3: UILabel!
+    
+    //MARK: Network Speed Labels
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    
+    
+    //MARK: Port Labels
+    @IBOutlet weak var port21: UILabel!
+    @IBOutlet weak var port22: UILabel!
+    @IBOutlet weak var port23: UILabel!
+    @IBOutlet weak var port25: UILabel!
+    @IBOutlet weak var port53: UILabel!
+    @IBOutlet weak var port80: UILabel!
+    @IBOutlet weak var port110: UILabel!
+    @IBOutlet weak var port1433: UILabel!
+    @IBOutlet weak var port3389: UILabel!
+    @IBOutlet weak var port5900: UILabel!
+    
+    
+    @IBOutlet weak var port115: UILabel!
+    @IBOutlet weak var port135: UILabel!
+    @IBOutlet weak var port139: UILabel!
+    @IBOutlet weak var port143: UILabel!
+    @IBOutlet weak var port194: UILabel!
+    @IBOutlet weak var port443: UILabel!
+    @IBOutlet weak var port445: UILabel!
+    @IBOutlet weak var port3306: UILabel!
+    @IBOutlet weak var port5632: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
